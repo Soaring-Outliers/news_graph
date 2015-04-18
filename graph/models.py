abc45951f339b192464b9a2c4a2052c72ea0d230
pidupuis <pierredupuis@openmailbox.org>
2015-04-18 20:45:52 +0000

    Add bower; Begin models

--- graph/models.py
+++ graph/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Website(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    rss_url = models.CharField(max_length=255)
