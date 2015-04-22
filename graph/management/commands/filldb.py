9a60a053ae59b255817839be75b7f3481839ce3e
pidupuis <pierredupuis@openmailbox.org>
2015-04-22 19:33:26 +0000

    Update cron

--- graph/management/commands/filldb.py
+++ graph/management/commands/filldb.py
@@ -2,8 +2,9 @@ from django.core.management.base import BaseCommand, CommandError
 from graph.models import Website
 
 class Command(BaseCommand):
-	help = 'Fill database by retrieving articles and concepts'
+    help = 'Fill database by retrieving articles and concepts'
 
-	def handle(self, *args, **options):
-	    for website in Website.objects.all():
-	        website.download_rss_articles()
+    def handle(self, *args, **options):
+        for website in Website.objects.all():
+            print(website.name)
+            website.download_rss_articles()
